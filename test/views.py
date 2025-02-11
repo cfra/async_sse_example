34814057b412c92de4564ad7a8cdaa5af907f5d5
Christian Franke <nobody@nowhere.ws>
2025-02-11 11:51:46 +0100

    Fix the issue and document solution

--- test/views.py
+++ test/views.py
@@ -7,19 +7,27 @@ from rest_framework.renderers import BaseRenderer
 from rest_framework.response import Response
 
 
+class ServerSentEventResponse(StreamingHttpResponse):
+    """
+    A streamed response configured to send Server Sent Events.
+    """
+    def __init__(self, *args, **kwargs):
+        kwargs['content_type'] = 'text/event-stream'
+        super().__init__(*args, **kwargs)
+        self['X-Accel-Buffering'] = 'no'
+        self['Cache-Control'] = 'no-cache'
+
+
 class ServerSentEventRenderer(BaseRenderer):
     """
-    Pass through StreamingHttpResponse unchanged.
+    A renderer for server sent events. This renderer is just used for
+    content negotiation, as the ServerSentEventResponse doesn't have
+    a render function the renderer will never be called.
     """
     media_type = 'text/event-stream'
     format = 'txt'
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        traceback.print_stack()
-        breakpoint()
-        response = StreamingHttpResponse(data, content_type='text/event-stream')
-        response['X-Accel-Buffering'] = 'no'
-        response['Cache-Control'] = 'no-cache'
-        return response
+        assert False
 
 
 async def example_events():
@@ -32,10 +40,9 @@ class TestViewSet(ViewSet):
     renderer_classes = [ServerSentEventRenderer]
 
     async def list(self, request):
-        return Response(example_events())
+        return ServerSentEventResponse(example_events())
 
 
 async def test_view(request):
-    renderer = ServerSentEventRenderer()
     data = example_events()
-    return renderer.render(data)
+    return ServerSentEventResponse(example_events())
